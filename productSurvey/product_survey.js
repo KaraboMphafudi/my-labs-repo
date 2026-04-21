ba98614d2db3c567fba5ebdbc1eb387e70de376f
KaraboMphafudi <Karabo.ekfm@gmail.com>
2026-04-21 04:20:27 -0400

    added experience textarea to product survey

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -7,7 +7,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
-    
+    const experience = document.getElementById('userExperince').value;
     // Step 3: Show thank you message
     alert('Thank you for your valuable feedback');
 
@@ -18,6 +18,7 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperience').innerHTML = experience;
 
     document.getElementById('userInfo').style.display = 'block';
 
