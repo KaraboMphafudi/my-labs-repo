b92d05b1b388afc69c0e8c7ab4f915bc6ac1f625
KaraboMphafudi <Karabo.ekfm@gmail.com>
2026-05-01 17:32:56 -0400

    Add weatherReport folder with HTML and JS files

---
+++ weatherReport/weather_report.js
@@ -0,0 +1,5 @@
+// Change this:
+var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6fbbd5c9597da9a6a7826459c00513fa`;
+
+// To this:
+var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=YOUR_API_KEY`;
